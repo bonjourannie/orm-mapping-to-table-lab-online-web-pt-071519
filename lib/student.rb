787246db10ff6ce163cff609ff94146a46139057
Annie Truex <artruex@gmail.com>
2019-10-04 00:36:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
     @id = id
   end
   
-  def seld.create_table
+  def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
